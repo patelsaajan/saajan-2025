c15636ccc3bd0c7146d68703ba770cc51baf44e4
patelsaajan <saajanpatel@hotmail.com>
2025-08-23 21:14:29 +0100

    feat: frontend of contact form

--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -10,7 +10,7 @@
         <UFormField label="Phone" name="phone" class="col-span-2">
           <UInput v-model="state.phone" class="flex"/>
         </UFormField>
-
+        
         <UFormField label="Email" name="email" class="col-span-4">
           <UInput v-model="state.email"  class="flex"/>
         </UFormField>
@@ -19,13 +19,22 @@
           <UInput v-model="state.subject" class="flex"/>
         </UFormField>
 
-        <UFormField label="Subject" name="subject" class="col-span-4">
-          <UTextarea v-model="state.subject" class="flex" rows="5"/>
+        <UFormField label="Message" name="message" class="col-span-4">
+          <UTextarea v-model="state.message" class="flex" rows="5"/>
         </UFormField>
 
 
         <div class="col-span-4">
-          <UButton type="submit">Submit</UButton>
+          <UButton 
+            type="submit"
+            icon="i-lucide-rocket"
+            loading-auto
+            loading-icon="i-lucide-loader"
+            class="cursor-pointer w-full justify-center"
+            variants="primary"
+          >
+          Submit
+        </UButton>
         </div>
       </div>
     </UForm>
@@ -35,34 +44,60 @@
 <script setup lang="ts">
 import * as z from 'zod'
 import type { FormSubmitEvent } from '@nuxt/ui'
-import { UTextarea } from '#components';
 
 const schema = z.object({
-  name: z.string().min(2, 'Must be at least 2 characters'),
-  email: z.string().email('Invalid email'),
+  name: z.string()
+  .min(2, { error: "Must be at least 2 characters" }),
+
+  email: z.string()
+    .email({ error: "Must be a valid email format" }),
+
   phone: z.string()
     .regex(
       /^(?:\+44\s?7\d{3}|\(?07\d{3}\)?)\s?\d{3}\s?\d{3}$/,
-      'Invalid UK phone number'
-    ).optional(),
-  subject: z.string().min(5, 'Must be at least 5 characters'),
-  message: z.string().min(15, 'Must be at least 15 characters')
+      { error: "Invalid UK phone number" }
+    )
+    .optional()
+    .or(z.literal("")),
+
+  subject: z.string()
+    .min(5, { error: "Must be at least 5 characters" }),
+
+  message: z.string()
+    .min(10, { error: "Must be at least 10 characters" }),
+  
+  lastName: z.string().optional()
 });
+
 
 
 type Schema = z.output<typeof schema>
 
 const state = reactive<Partial<Schema>>({
-  email  : undefined,
-  phone  : undefined,
-  subject: undefined,
-  message: undefined
+  name    : '',
+  email   : '',
+  lastName: "",
+  phone   : '',
+  subject : '',
+  message : ''
 })
 
 const toast = useToast()
 async function onSubmit(event: FormSubmitEvent<Schema>) {
-  toast.add({ title: 'Success', description: 'The form has been submitted.', color: 'success' })
-  console.log(event.data)
+  if (state.lastName) {
+    toast.add({
+      title      : 'Error',
+      description: 'Invalid Form Submission',
+      color      : 'error',
+      icon       : 'subway:error'
+    })
+    return
+  }
+
+  setTimeout(() => {
+    toast.add({ title: 'Success', description: 'Your form has been submitted successfuly.', color: 'success' })
+    console.log(event.data)
+  }, 250);
 }
 </script>
 
